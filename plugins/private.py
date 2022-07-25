a334352b87fb02782e17a46ec01a47100982f3d7
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2022-07-25 21:44:02 +0530

    Update private.py

--- plugins/private.py
+++ plugins/private.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 @Client.on_message(filters.private & filters.incoming)
 async def private_link_handler(c, message):
     if message.from_user.id not in ADMINS:
-        return await message.reply_text(f"This bot works only for ADMINS of this bot. Make your own Bot")
+        return await message.reply_text(f"This bot works only for ADMINS of this bot. Make your own Bot contact - @potter_00")
         
     user_method = await db.get_bot_method(temp.BOT_USERNAME)
 
